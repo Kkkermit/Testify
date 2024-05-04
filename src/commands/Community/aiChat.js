8e73e05f10159139723dd14ca3e40398c00e70f2
Kkkermit <jeaton446@gmail.com>
2024-05-04 14:47:43 +0100

    update: Updated aiChat command to allow the use of multiple modals as turbo as default

--- src/commands/Community/aiChat.js
+++ src/commands/Community/aiChat.js
@@ -5,17 +5,22 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName('chat')
     .setDescription('Generate AI chat response')
-    .addStringOption(option => option.setName('prompt').setDescription('The prompt for gpt').setRequired(true)),
+    .addStringOption(option => option.setName('prompt').setDescription('The prompt for gpt').setRequired(true))
+    .addStringOption(option => option.setName('modal').setDescription('The modal to use')
+    .addChoices(
+            {name: "v3-32k", value: "v3-32k"},
+            {name: "turbo", value: "turbo"},
+            {name: "apexChat", value: "apexChat"},
+            {name: "starChat", value: "starChat"},
+            {name: "check docs", value: "check docs"}).setRequired(false)),
     async execute(interaction) {
 
     await interaction.deferReply()
-    const modal = interaction.options.getString('modal');
+    const modal = interaction.options.getString('modal') || "turbo";
     const prompt = interaction.options.getString('prompt');
 
-    // Modals: v3-32k, turbo, apexChat, starChat, check docs //
-
         try {
-            const response = await ApexChat("turbo", prompt);
+            const response = await ApexChat(modal, prompt);
 
             await interaction.editReply({ content: `${response}` });
         } catch (error) {
