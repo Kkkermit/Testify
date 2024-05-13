e47f392137c91e60dc8ae0254b421a60d7232a7a
Kkkermit <jeaton446@gmail.com>
2024-05-13 19:41:57 +0100

    add: Added permissions tracker command

---
+++ src/commands/InfoCommands/permissionTracker.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("permissions")
+    .setDescription("Displays Permissions")
+    .addUserOption(option => option.setName("user").setDescription("The user to get permissions for").setRequired(false)),
+    
+    async execute(interaction, client) {
+
+        const { options } = interaction;
+
+        const member = options.getMember("user") || interaction.member;
+        const user = options.getUser("user") || interaction.user;
+
+        if (!member) return interaction.reply({ content: "Member **could not** be found!", ephemeral: true,});
+  
+        let permissionFlags = Object.keys(PermissionFlagsBits);
+
+        let output = `• Permissions for **${member}**  \n\`\`\``;
+        for (let i = 0; i < permissionFlags.length; i++) {
+            let permissionName = permissionFlags[i];
+            let hasPermission = member.permissions.has(permissionName);
+            output += `${permissionName} ${hasPermission ? "✅" : "❌"}\n`;
+        }
+        output += `\`\`\``;
+        
+        const PermsEmbed = new EmbedBuilder()
+        .setTitle(`${client.user.username} Permissions Tracker ${client.config.arrowEmoji}`)
+        .setAuthor({ name: `Permissions Tracker ${client.config.devBy}`})
+        .setDescription(`> **${user.tag}** permissions in **${interaction.guild.name}** \n\n${output}`)
+        .setColor(client.config.embedInfo)
+        .setThumbnail(user.avatarURL())
+        .setFooter({ text: `${user.tag} permissions`})
+        .setTimestamp();
+        
+        return interaction.reply({ embeds: [PermsEmbed] });
+    },
+};
