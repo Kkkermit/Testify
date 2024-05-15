caff99ecbd360c603d81a94af5df4567ce5c7c36
Kkkermit <jeaton446@gmail.com>
2024-05-15 02:05:22 +0100

    update: Update hackUser.js storing info into a json file

--- src/commands/Fun/hackUser.js
+++ src/commands/Fun/hackUser.js
@@ -20,7 +20,7 @@ module.exports = {
         await wait(2500);
         await interaction.editReply({ content: `Getting **${target}** devices password and ID..` })
         await wait(2500);
-        await interaction.editReply({ content: `Stealing **${target}** mom credit card..` })
+        await interaction.editReply({ content: `Stealing **${target}** moms credit card..` })
         await wait(2500);
         await interaction.editReply({ content: `Hacking **${target}** computer and Wi-Fi..` })
         await wait(2500);
@@ -30,6 +30,7 @@ module.exports = {
         await wait(3000);
         await interaction.editReply({ content: `Mission complete! I've successfully hacked **${target}** devices, and exposed everything he has!` })
 
+        const data = require('../../jsons/hackUsers.json')
 
         let devicePassword = [        
             `${target.tag}845!!`,
@@ -42,54 +43,10 @@ module.exports = {
         ]
 
         let HackDevicePassword = devicePassword[Math.floor(Math.random() * devicePassword.length)];
-
-        let id = [
-            `1234567890`,
-            `0987654321`,
-            `5432167890`,
-            `4673456783`,
-            `1295674377`,
-            `2364784236`,
-            `7985644738`
-        ]
-
-        let HackId = id[Math.floor(Math.random() * id.length)];
-
-        const wifiName = [
-            `SKY485hd3`,
-            `TFGS36H75`,
-            `SKY295GH9`,
-            `EE23HGD64`,
-            `TG67J5G43`,
-            `SKY11LLS4`,
-            `EEKF45H54`,
-        ]
-
-        let HackWifiName = wifiName[Math.floor(Math.random() * wifiName.length)];
-
-        let wifiPassword = [
-            `Tgs35Jf4`,
-            `Jg5Hf4J5`,
-            `Hltg567h`,
-            `FFjj3j36`,
-            `Pp5Jg5J5`,
-            `34PoImmf`,
-            `Qgr34671`
-        ]
-
-        let HackWifiPassword = wifiPassword[Math.floor(Math.random() * wifiPassword.length)];
-
-        let location  = [
-            `12 Pretty Dr \nScole \nDiss \nIP21 4DG`,
-            `23 Green Rd \nHalesworth \nIP19 8JH`,
-            `45 Blue St \nEye \nIP23 7JG`,
-            `67 Red Rd \nHarleston \nIP20 9JF`,
-            `89 Yellow St \nBeccles \nNR34 9JG`,
-            `90 Orange Rd \nLowestoft \nNR32 9JH`,
-            `11 Purple St \nBungay \nNR35 9JF`
-        ]
-
-        let HackLocation = location[Math.floor(Math.random() * location.length)];
+        let HackId = data.id[Math.floor(Math.random() * data.id.length)];
+        let HackWifiName = data.wifiName[Math.floor(Math.random() * data.wifiName.length)];
+        let HackWifiPassword = data.wifiPassword[Math.floor(Math.random() * data.wifiPassword.length)];
+        let HackLocation = data.location[Math.floor(Math.random() * data.location.length)];
 
         let passwords = [
             `Ghd46zh1 \nHltg567h \nAdmin \nPassword123 \n${target.tag}845!!`,
@@ -113,33 +70,11 @@ module.exports = {
         ]
 
         let HackEmail = email[Math.floor(Math.random() * email.length)];
-
-        let dob = [
-            `31/02/2005`,
-            `12/04/2001`,
-            `23/05/2000`,
-            `01/01/2001`,
-            `21/05/2005`,
-            `05/05/2005`,
-            `01/01/2005`
-        ]
-
-        let HackDob = dob[Math.floor(Math.random() * dob.length)];
-
-        let creditCard = [
-            `Credit Card: 1234 5678 9101 1121 \nExpiry Date: 12/23 \nCVV: 123`,
-            `Credit Card: 4321 8765 1098 2111 \nExpiry Date: 11/22 \nCVV: 321`,
-            `Credit Card: 5678 1234 2111 1098 \nExpiry Date: 10/21 \nCVV: 456`,
-            `Credit Card: 8765 4321 1211 9876 \nExpiry Date: 09/20 \nCVV: 789`,
-            `Credit Card: 9876 5432 2111 8765 \nExpiry Date: 08/19 \nCVV: 654`,
-            `Credit Card: 6543 2109 1112 5432 \nExpiry Date: 07/18 \nCVV: 987`,
-            `Credit Card: 3210 9876 2111 2345 \nExpiry Date: 06/17 \nCVV: 321`
-        ]
-
-        let HackCreditCard = creditCard[Math.floor(Math.random() * creditCard.length)];
+        let HackDob = data.dob[Math.floor(Math.random() * data.dob.length)];
+        let HackCreditCard = data.creditCard[Math.floor(Math.random() * data.creditCard.length)];
 
         const embed = new EmbedBuilder()
-        .setAuthor({ name: `${client.user.username} Hacking System`})
+        .setAuthor({ iconURL: client.user.displayAvatarURL({ dynamic: true}), name: `${client.user.username} Hacking System`})
         .setColor(client.config.embedFun)
         .setTitle(`**${target.tag}'s** data ${client.config.arrowEmoji}`)
         .addFields(
