140a4ec70b0c1c8e44f0938f712e3e8e999dea7e
Kkkermit <jeaton446@gmail.com>
2024-05-16 02:02:47 +0100

    add: Added a filter.json and ascii art command

---
+++ src/commands/Fun/ascii.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const figlet = require('figlet')
+const filter = require('../../jsons/filter.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ascii')
+    .setDescription('Converts text into ascii art.')
+    .addStringOption(option => option.setName('text').setDescription('Specified text will be converted into art.').setRequired(true).setMaxLength(15)),
+    async execute(interaction, client) {
+        const text = interaction.options.getString('text')
+
+        figlet(`${text}`, function (err, data) {
+
+            if (err) {
+                return interaction.reply({ content: `Something has gone wrong, please try again!`, ephemeral: true})
+            }
+
+            if (filter.words.includes(text)) return interaction.reply({ content: `We cannot send your message as it includes **explicit** language`, ephemeral: true});
+
+            const embed = new EmbedBuilder()
+            .setColor(client.config.embedFun)
+            .setTimestamp()
+            .setDescription(`\`\`\`${data}\`\`\``)
+
+            interaction.reply({ embeds: [embed] });
+        
+        });
+    }
+}
