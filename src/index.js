9c362fc668365f06c9c9bab251486298566ee7da
Kkkermit <jeaton446@gmail.com>
2024-05-16 22:33:40 +0100

    update: Updated status

--- src/index.js
+++ src/index.js
@@ -53,10 +53,11 @@ client.on("ready", async (client) => {
     setInterval(() => {
 
         let activities = [
-            { type: 'Watching', name: `${client.commands.size} commands!`},
+            { type: 'Watching', name: `${client.commands.size} slash commands!`},
             { type: 'Watching', name: `${client.pcommands.size} prefix commands!`},
             { type: 'Watching', name: `${client.guilds.cache.size} servers!`},
             { type: 'Watching', name: `${client.guilds.cache.reduce((a,b) => a+b.memberCount, 0)} members!`},
+            { type: 'Playing', name: `${client.config.prefix}help | @${client.user.username}`},
         ];
 
         const status = activities[Math.floor(Math.random() * activities.length)];
