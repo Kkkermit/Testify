596594c2addd243e8304a66c8912dcc38055a6c2
Kkkermit <jeaton446@gmail.com>
2024-05-18 15:53:20 +0100

    fix: Fixed kick and ban prefix commands

--- src/prefix/HardModeration/ban.js
+++ src/prefix/HardModeration/ban.js
@@ -3,14 +3,26 @@ const { EmbedBuilder, PermissionsBitField } = require("discord.js")
 module.exports = {
     name: 'ban',
     async execute(message, client, args)  {
+        
         const user = message.guild.members.cache.get(args[1]) || message.mentions.members.first() 
 
         if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) return await message.channel.send({ content: `${client.config.noPerms}`, ephemeral: true});
         if (!user) return message.channel.send({ content: `You need to mention a **user** to ban!`, ephemeral: true})
         const reason = args.slice(1).join(' ') || '\`\`No reason provided\`\`'
 
-        try {
-            await user.ban([reason])
+        if (user.bannable) {
+
+            const dmEmbed = new EmbedBuilder()
+            .setAuthor({ name: `${client.user.username} ban command` })
+            .setColor(client.config.embedModHard)
+            .setTitle(`> ${client.config.modEmojiHard}  Ban command ${client.config.arrowEmoji}`)
+            .setDescription(`\n ${user}, \n \`You have been banned from ${message.channel.guild}\` \n \n \n **Reason:** \n ${reason} \n \n **Staff Member:** \n ${message.member} | (<@${message.member.id}>:${message.member.id}) \n`)
+            .setTimestamp()
+            .setThumbnail(client.user.avatarURL())
+            .setFooter({ text: `Banned - ${message.channel.guild} ${client.config.devBy}` });
+
+            user.send({ embeds: [dmEmbed] })
+            user.ban({ reason: reason })
 
             const banSuccess = new EmbedBuilder()
             .setAuthor({ name: `${client.user.username} ban command ${client.config.devBy}` })
@@ -22,37 +34,13 @@ module.exports = {
             .setThumbnail(client.user.avatarURL())
             .setFooter({ text: `Someone got got struck by the ban hammer` })
 
-            message.channel.send({ embeds : [banSuccess] }).catch(err => {
-                return;
-            });
+            message.channel.send({ embeds : [banSuccess] })
 
-        } catch (error) {
-            console.log(error)
-            message.channel.send({ content: `Failed to ban **${user}**!`, ephemeral: true}).catch(err => {
-                return;
-            });
-        } try {
-
-            const dmEmbed = new EmbedBuilder()
-            .setAuthor({ name: `${client.user.username} ban command` })
+        } else { 
+            const Failed = new EmbedBuilder()
+            .setDescription(`Failed to ban **${user}**!`)
             .setColor(client.config.embedModHard)
-            .setTitle(`> ${client.config.modEmojiHard}  Ban command ${client.config.arrowEmoji}`)
-            .setDescription(`\n ${user}, \n \`You have been banned from ${message.channel.guild}\` \n \n \n **Reason:** \n ${reason} \n \n **Staff Member:** \n ${message.member} | (<@${message.member.id}>:${message.member.id}) \n`)
-            .setTimestamp()
-            .setThumbnail(client.user.avatarURL())
-            .setFooter({ text: `Banned - ${message.channel.guild} ${client.config.devBy}` });
-
-            message.user.send({ embeds: [dmEmbed] }).catch(err => {
-                return;
-            });
-        } catch (error) {
-            console.log(error)
-
-            const dmFailed = new EmbedBuilder()
-            .setDescription(`Could not DM **${user}**!`)
-            .setColor(client.config.embedModHard)
-
-            message.channel.send({ embeds: [dmFailed], ephemeral: true}).catch(err => {
+            message.channel.send({ embeds: [Failed], ephemeral: true}).catch(err => {
                 return;
             });
         }
---
+++ src/prefix/HardModeration/kick.js
@@ -0,0 +1,50 @@
+const { EmbedBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    name: 'kick',
+    aliases: ['boot'],
+    async execute(message, client, args) {
+
+        const user = message.guild.members.cache.get(args[1]) || message.mentions.members.first() 
+
+        if (!message.member.permissions.has(PermissionFlagsBits.KickMembers)) return message.reply({ content: `${client.config.noPerms}`, ephemeral: true });
+        if (!user) return message.reply({ content: 'Please mention a **user** to kick.', ephemeral: true });
+
+        const reason = args.slice(1).join(' ') || `\`\`No reason given\`\``;
+
+        if (user.kickable) {
+
+            const dmEmbed = new EmbedBuilder()
+            .setAuthor({ name: `${client.user.username} kick command` })
+            .setColor(client.config.embedModHard)
+            .setTitle(`> ${client.config.modEmojiHard}  Kick command ${client.config.arrowEmoji}`)
+            .setDescription(`\n ${user}, \n \`You have been kicked from ${message.channel.guild}\` \n \n \n **Reason:** \n ${reason} \n \n **Staff Member:** \n ${message.member} | (<@${message.member.id}>:${message.member.id}) \n`)
+            .setTimestamp()
+            .setThumbnail(client.user.avatarURL())
+            .setFooter({ text: `Kicked - ${message.channel.guild} ${client.config.devBy}` });
+    
+            user.send({ embeds: [dmEmbed] })
+            user.kick({ reason: reason })
+
+            const kickEmbed = new EmbedBuilder()
+            .setAuthor({ name: `${client.user.username} kick command`})
+            .setTitle(`> ${client.config.modEmojiHard}  Kick command ${client.config.arrowEmoji}`)
+            .setColor(client.config.embedModHard)
+            .addFields({ name: 'User', value: `> ${user}`, inline: true })
+            .addFields({ name: 'Reason', value: `> ${reason}`, inline: true })
+            .setFooter({ text: `Someone got kicked from the server` })
+            .setThumbnail(client.user.avatarURL())
+            .setTimestamp()
+
+            message.channel.send({ embeds: [kickEmbed]})
+
+        } else {
+            const Failed = new EmbedBuilder()
+            .setDescription(`Failed to kick **${user}**!`)
+            .setColor(client.config.embedModHard)
+            message.channel.send({ embeds: [Failed], ephemeral: true}).catch(err => {
+                return;
+            });
+        }
+    }
+}
