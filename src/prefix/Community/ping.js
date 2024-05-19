a437d09fe30ed7af3babeb9bf7d5eea3616052bc
Kkkermit <jeaton446@gmail.com>
2024-05-19 04:30:21 +0100

    add: Added ping command for basic testing

---
+++ src/prefix/Community/ping.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require('discord.js'); 
+
+module.exports = {
+    name: 'ping',
+    async execute(message, client) {
+
+        const embed = new EmbedBuilder()
+        .setAuthor({ name: `${client.user.username} ${client.config.devBy}`})
+        .setTitle(`${client.user.username} **ping** command`)
+        .setDescription(`> Pong! ${client.ws.ping}ms`)
+        .setColor(client.config.embedCommunity)
+        .setFooter({ text: `Requested by ${message.author.username}`, iconURL: message.author.avatarURL() })
+        .setTimestamp()
+
+        message.channel.send({ embeds: [embed] });
+    }
+}
