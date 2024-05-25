99ca7d0ad51c3b148d9098ca7c68477bcdfe1f39
Kkkermit <jeaton446@gmail.com>
2024-05-25 03:46:17 +0100

    update: Updated messageCreate to provide error message

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,19 +1,15 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: "messageCreate",
     async execute(message, client) {
         if (
-            message.author.bot ||
-            !message.guild ||
-            message.system ||
-            message.webhookId
+            message.author.bot || !message.guild || message.system || message.webhookId
         )
     return;
 
     if (!message.content.startsWith(client.config.prefix)) return;
-        const args = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(/ +/);
+        const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
 
     let cmd = args.shift().toLowerCase();
         if (cmd.length === 0) return;
@@ -30,7 +26,13 @@ module.exports = {
         try {
             command.execute(message, client, args);
         } catch (error) {
-            client.logs.error(error);
+            client.logs.error(error)
+
+            const embed = new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(`There was an error while executing this command!\n\`\`\`${error}\`\`\``)
+
+            await message.reply({ embeds: [embed], ephemeral: true});
         }
     },
 };
