a13d92034d2356b55ccb533958dac3264ccee552
Kkkermit <jeaton446@gmail.com>
2024-06-02 23:39:51 +0100

    add: Added console logging

--- src/events/ready.js
+++ src/events/ready.js
@@ -20,6 +20,29 @@ module.exports = {
             useUnifiedTopology: true,
         })
 
+        const color = {
+            red: '\x1b[31m',
+            orange: '\x1b[38;5;202m',
+            yellow: '\x1b[33m',
+            green: '\x1b[32m',
+            blue: '\x1b[34m',
+            pink: '\x1b[38;5;213m',
+            torquise: '\x1b[38;5;45m',
+            purple: '\x1b[38;5;57m',
+            reset: '\x1b[0m'
+        }
+        
+        function getTimestamp() {
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+            const seconds = date.getSeconds();
+            return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        };
+
         if (mongoose.connect) {
             client.logs.success('[DATABASE] Connected to MongoDB successfully.')
 
@@ -32,6 +55,15 @@ module.exports = {
                 client.logs.success(`[SCHEMAS] Loaded ${files.length} schema files.`);
             });
         }
+
+        console.log(`${color.pink}[${getTimestamp()}] ======================================================`)
+        console.log(`${color.pink}[${getTimestamp()}] ████████╗███████╗███████╗████████╗██╗███████╗██╗   ██╗`)
+        console.log(`${color.pink}[${getTimestamp()}] ╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝██║██╔════╝╚██╗ ██╔╝`)
+        console.log(`${color.pink}[${getTimestamp()}]    ██║   █████╗  ███████╗   ██║   ██║█████╗   ╚████╔╝ `)
+        console.log(`${color.pink}[${getTimestamp()}]    ██║   ██╔══╝  ╚════██║   ██║   ██║██╔══╝    ╚██╔╝  `)
+        console.log(`${color.pink}[${getTimestamp()}]    ██║   ███████╗███████║   ██║   ██║██║        ██║   `)
+        console.log(`${color.pink}[${getTimestamp()}]    ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚═╝╚═╝        ╚═╝   `)
+        console.log(`${color.pink}[${getTimestamp()}] ======================================================`) 
 
         client.logs.logging(`[BOT] ${client.user.username} has been launched!`);
         client.logs.info(`[EVENTS] Started loading events...`)
