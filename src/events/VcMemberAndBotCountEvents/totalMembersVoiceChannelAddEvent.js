c9cd7816706adf1e887bfbde9803d39db1e08d89
Kkkermit <jeaton446@gmail.com>
2024-08-04 16:27:31 +0100

    fix: Fixed vc stats events

--- src/events/VcMemberAndBotCountEvents/totalMembersVoiceChannelAddEvent.js
+++ src/events/VcMemberAndBotCountEvents/totalMembersVoiceChannelAddEvent.js
@@ -19,22 +19,3 @@ module.exports = {
         }
     }
 }
-
-module.exports = {
-    name: Events.GuildMemberRemove,
-    async execute (member, client, err) {
-
-        if (member.guild === null) return;
-        const voiceData1 = await voiceSchema.findOne({ Guild: member.guild.id });
-
-        if (!voiceData1) return;
-        else {
-
-            const totalVoiceChannel1 = member.guild.channels.cache.get(voiceData1.TotalChannel);
-            if (!totalVoiceChannel1 || totalVoiceChannel1 === null) return;
-            const totalMembers1 = member.guild.memberCount;
-
-            totalVoiceChannel1.setName(`• Total Members: ${totalMembers1}`).catch(err);
-        }
-    }
-}
