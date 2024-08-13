c49a50029fe8c9a8dc4cf636cfb7c2559b177da4
Kkkermit <jeaton446@gmail.com>
2024-08-14 00:46:12 +0100

    add: Added the choice of being able to boot into development mode or normal mode

--- src/index.js
+++ src/index.js
@@ -11,7 +11,52 @@
 
 const { Client, GatewayIntentBits, EmbedBuilder, Collection, Partials } = require(`discord.js`);
 const fs = require('fs');
+const path = require('path');
 const config = require('./config')
+
+// Logging Effects //
+
+const color = {
+    red: '\x1b[31m',
+    orange: '\x1b[38;5;202m',
+    yellow: '\x1b[33m',
+    green: '\x1b[32m',
+    blue: '\x1b[34m',
+    pink: '\x1b[38;5;213m',
+    torquise: '\x1b[38;5;45m',
+    purple: '\x1b[38;5;57m',
+    reset: '\x1b[0m'
+}
+
+function getTimestamp() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
+const dotenv = require('dotenv');
+
+const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env';
+const envPath = path.resolve(process.cwd(), envFile);
+
+console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Loading environment variables from: ${envPath}`);
+if (process.env.NODE_ENV === 'development') { 
+    console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] ${process.env.NODE_ENV} mode has been loaded!`);
+} else {
+    console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Non-development mode has been loaded!`);
+}
+
+if (fs.existsSync(envPath)) {
+    dotenv.config({ path: envPath });
+} else {
+    console.error(`Environment file ${envFile} not found`);
+    process.exit(1);
+}
 
 // Version Control //
 
@@ -99,31 +144,6 @@ const commandFolders = fs.readdirSync("./src/commands");
         console.error(`${color.red}[${getTimestamp()}]${color.reset} [LOGIN] Error while logging in. Check if your token is correct or double check your also using the correct intents. \n${color.red}[${getTimestamp()}]${color.reset} [LOGIN]`, error);
     });
 })();
-
-// Logging Effects //
-
-const color = {
-    red: '\x1b[31m',
-    orange: '\x1b[38;5;202m',
-    yellow: '\x1b[33m',
-    green: '\x1b[32m',
-    blue: '\x1b[34m',
-    pink: '\x1b[38;5;213m',
-    torquise: '\x1b[38;5;45m',
-    purple: '\x1b[38;5;57m',
-    reset: '\x1b[0m'
-}
-
-function getTimestamp() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-};
 
 // Music System //
 
