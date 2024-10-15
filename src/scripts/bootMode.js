c8e9c1694ceaec155adafc5a0da3d7615923e37b
Kkkermit <jeaton446@gmail.com>
2024-10-15 21:35:06 +0100

    update: Updated ytdl-core version, updated color terminals to include color.reset

--- src/scripts/bootMode.js
+++ src/scripts/bootMode.js
@@ -7,17 +7,17 @@ function loadEnvironment() {
     const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env';
     const envPath = path.resolve(process.cwd(), envFile);
 
-    console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Loading environment variables from: ${envPath}`);
+    console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Loading environment variables from: ${envPath}${color.reset}`);
     if (process.env.NODE_ENV === 'development') { 
-        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] ${process.env.NODE_ENV} mode has been loaded!`);
+        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] ${process.env.NODE_ENV} mode has been loaded!${color.reset}`);
     } else {
-        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Non-development mode has been loaded!`);
+        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Non-development mode has been loaded!${color.reset}`);
     }
 
     if (fs.existsSync(envPath)) {
         dotenv.config({ path: envPath });
     } else {
-        console.error(`${color.red}[${getTimestamp()}] [ERROR] Environment file ${envFile} not found`, error);
+        console.log(`${color.red}[${getTimestamp()}] [ERROR] Environment file ${envFile} not found${color.reset}`);
         process.exit(1);
     }
 }
