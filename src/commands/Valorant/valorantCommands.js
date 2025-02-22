83189bdba892296c793d6eaea0b49e79c9e2fce5
Loxt_Nixo <128501463+loxt-nixo@users.noreply.github.com>
2025-02-22 16:45:44 +0100

    Update valorantCommands.js

--- src/commands/Valorant/valorantCommands.js
+++ src/commands/Valorant/valorantCommands.js
@@ -3,6 +3,10 @@ const ValoAPI = require('../../api/valorantApi');
 const ValorantUser = require('../../schemas/valorantUserSystem');
 const images = require('../../images');
 
+function addSpaceBeforeCapitalized(str) {
+    return str.replace(/([a-z])([A-Z])/g, '$1 $2');
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('valorant')
@@ -124,37 +128,79 @@ module.exports = {
 
                 break;
                 case 'search-skin':
-
                     const skinUUID = interaction.options.getString('name');
-                    const foundSkin = client.skins.find(s => s["uuid"] === skinUUID);
-    
-                    if (!foundSkin) {
-                        return interaction.reply({ 
-                            content: 'Skin not found!', 
-                            ephemeral: true 
-                        });
-                    }
-    
-                    const skinTier = client.skinsTier.find(s => s["uuid"] === foundSkin["contentTierUuid"]) || {
-                        highlightColor: "808080FF",
-                        displayIcon: null,
-                        price: 0
-                    };
 
+                    const foundSkin = client.skins.find(s => s["uuid"] === skinUUID);
+            
+                    if (!foundSkin) return interaction.reply({ content: 'Skin not found!', hidden: true });
+            
+                    const skinTier = client.skinsTier.find(s => s["uuid"] === foundSkin["contentTierUuid"]);
+            
                     const skinPrice = await fetch("https://val-skin-price.vercel.app/beta/skins/" + skinUUID);
                     const skinPriceData = await skinPrice.json();
-    
+            
                     const skinEmbed = new EmbedBuilder()
-                        .setAuthor({ name: `Valorant Skin Search | Developed by arnsfh`, iconURL: "https://i.postimg.cc/RVzrNstM/arnsfh.webp" })
-                        .setTitle(`${client.user.username} Valorant Skin Search ${client.config.arrowEmoji}`)
-                        .setColor(`#${skinTier["highlightColor"].slice(0, -2)}` || "DarkerGrey")
-                        .setThumbnail(skinTier["displayIcon"])
-                        .setImage(foundSkin["levels"][0]["displayIcon"] || foundSkin["displayIcon"])
-                        .setFooter({ text: `Valorant Skin Search`, iconURL: interaction.user.displayAvatarURL() })
-                        .setTimestamp()
-                        .setDescription(`> **${foundSkin["displayName"]["en-US"]}** \n\nPrice: **${skinPriceData["price"]}** ${client.config.valoPoints}`);
-    
-                    await interaction.reply({ embeds: [skinEmbed] });
+                    .setColor(`#${skinTier["highlightColor"].slice(0, -2)}` || "DarkerGrey")
+                    .setTitle(foundSkin["displayName"]["en-US"])
+                    .setThumbnail(skinTier["displayIcon"])
+                    .setImage(foundSkin["levels"][0]["displayIcon"] || foundSkin["displayIcon"])
+                    .setDescription(`Price: ${skinPriceData["price"] || skinTier["price"] || 'Error!'} ${client.config.Emojis["ValoPoints"]}`)
+            
+                    const SkinChromas = new ActionRowBuilder()
+                    const SkinLevels = new ActionRowBuilder()
+                    const Previews = new ActionRowBuilder()
+                    
+                    let i = 0;
+            
+                    for (const chroma of foundSkin["chromas"]) {
+                        const chromaEmojiData = client.swatch.find(s => s.uuid == chroma["uuid"]);
+            
+                        const ChromaButton = new ButtonBuilder()
+                        .setCustomId(`skin-chroma_${skinUUID}_${i}`)
+                        .setStyle(ButtonStyle.Primary)
+                        .setLabel(`${i == 0 ? "Base Variant" : `Variant ${i}`}`)
+                        .setDisabled(i == 0 ? true : false)
+                        
+                        if (chromaEmojiData) {
+                            ChromaButton.setEmoji({ id: chromaEmojiData["emojiId"] });
+                        }
+            
+                        SkinChromas.addComponents(
+                            ChromaButton
+                        )
+                        i++;
+                    }
+            
+                    i = 0;
+            
+                    for (const level of foundSkin["levels"]) {
+                        const LevelItem = level["levelItem"] ? addSpaceBeforeCapitalized(level["levelItem"].split("::")[1]) : null;
+            
+                        SkinLevels.addComponents(
+                            new ButtonBuilder()
+                            .setCustomId(`skin-level_${skinUUID}_${i}`)
+                            .setStyle(ButtonStyle.Primary)
+                            .setLabel(`${i == 0 ? 'Base Level' : `Level ${i} - ${LevelItem}`}`)
+                            .setDisabled(i == 0 ? true : false)
+                        )
+                        i++;
+                    }
+            
+                    Previews.addComponents(
+                        new ButtonBuilder()
+                        .setCustomId(`skin-preview_chromas_${skinUUID}_0`)
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(foundSkin["levels"][0]["streamedVideo"] ? false : true)
+                        .setLabel(`Variant Preview`),
+            
+                       new ButtonBuilder()
+                       .setCustomId(`skin-preview_levels_${skinUUID}_0`)
+                       .setDisabled(foundSkin["levels"][0]["streamedVideo"] ? false : true)
+                       .setStyle(ButtonStyle.Primary)
+                       .setLabel(`Level Preview`)
+                    )
+            
+                    await interaction.reply({ embeds: [skinEmbed], components: [SkinChromas, SkinLevels, Previews] });
 
                 break;
                 case 'store':
