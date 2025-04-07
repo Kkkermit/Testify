c2d2598b86bb26998dd89e36f389310951a6519b
Kkkermit <jeaton446@gmail.com>
2025-04-07 18:31:58 +0100

    fix: Fixed bug in eval command with embed not sending

--- src/commands/Owner/eval.js
+++ src/commands/Owner/eval.js
@@ -7,26 +7,33 @@ module.exports = {
     async execute(interaction, client) {
 
         if (!client.config.developers.includes(interaction.user.id)) {
+
+            await interaction.reply({ content: `${client.config.ownerOnlyCommand}`, flags: MessageFlags.Ephemeral });
+            
             const embed = new EmbedBuilder()
             .setAuthor({ name: `${interaction.user.tag} has tried to run the eval command`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-            .setTitle(`Unauthorized Access Attempt Detected. ${interaction.user.tag} has tried to run the eval command but is not a developer.`)
-            .setDescription(`If this persists, consider **revoking their access** to ${client.user.username} with \`\`/blacklist add\`\`.`)
+            .setTitle(`Unauthorized Access Attempt Detected ${client.config.arrowEmoji}`)
+            .setDescription(`${interaction.user.tag} has tried to run the eval command but is not a developer.\n\n If this persists, consider **revoking their access** to ${client.user.username} with \`\`/blacklist add\`\`.`)
             .addFields({ name: "UserID", value: `${interaction.user.id}`, inline: true })
             .setColor(client.config.embedDev)
             .setTimestamp()
             .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
             .setFooter({ text: `Eval warning embed ${client.config.devBy}`, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
 
-            for (const developerId of client.config.developers) {
+            const developerIds = Array.isArray(client.config.developers) 
+                ? client.config.developers 
+                : [client.config.developers];
+
+            for (const devId of developerIds) {
                 try {
-                    const developer = await client.users.fetch(developerId);
-                    await developer.send({ embeds: [embed] });
-                } catch (error) {
-                    client.logs.error(`[EVAL_ERROR] Failed to send message to developer ${developerId}:`, error);
+                    const dev = await client.users.fetch(devId.toString().trim());
+                    await dev.send({ embeds: [embed] });
+                } catch (err) {
+                    client.logs.error(`[EVAL_ERROR] Failed to send eval warning embed to developer ${devId}: ${err}`);
                 }
             }
             
-            return await interaction.reply({ content: `${client.config.ownerOnlyCommand}`, flags: MessageFlags.Ephemeral,});
+            return;
         }
 
         const modal = new ModalBuilder()
